eca0818b8c556dc18da05abdeffdb435e57974eb
excilys <excilys@Desktop-0313>
2018-12-11 17:06:22 +0100

    Controllers implemented

--- src/main/java/com/excilys/servlet/AddComputerServlet.java
+++ src/main/java/com/excilys/servlet/AddComputerServlet.java
@@ -12,6 +12,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
 import com.excilys.mapper.ComputerMapper;
@@ -20,26 +23,28 @@ import com.excilys.model.Computer;
 import com.excilys.service.CompanyService;
 import com.excilys.service.ComputerService;
 
-@WebServlet("/addComputer")
-public class AddComputerServlet extends HttpServlet {
+@Controller
+@RequestMapping("/addComputer")
+public class AddComputerServlet {
 
-	private static final long serialVersionUID = 4450306511039154175L;
 	@Autowired
 	private ComputerService computerService;
 	@Autowired
 	private CompanyService companyService;
 	@Autowired
 	private ComputerMapper computerMapper;
-
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String doGet(HttpServletRequest request) throws ServletException, IOException {
 		List<Company> companies = companyService.getCompanies();
 		List<Long> companiesIds = new ArrayList<>();
 		companies.stream().forEach(company -> companiesIds.add(company.getId()));
 		request.setAttribute("companiesIds", companiesIds);
-		this.getServletContext().getRequestDispatcher("/WEB-INF/views/addComputer.jsp").forward(request, response);
+		return "addComputer";
 	}
 
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	@RequestMapping(method = RequestMethod.POST)
+	public String doPost(HttpServletRequest request) throws ServletException, IOException {
 		String name = request.getParameter("computerName");
 		String introduced = request.getParameter("introduced");
 		String discontinued = request.getParameter("discontinued");
@@ -47,12 +52,6 @@ public class AddComputerServlet extends HttpServlet {
 
 		Computer computer = computerMapper.mapUnique(name, introduced, discontinued, companyNumber);
 		computerService.createComputer(computer);
-		this.getServletContext().getRequestDispatcher("/WEB-INF/views/addComputer.jsp").forward(request, response);
-	}
-
-	@Override
-	public void init(ServletConfig config) throws ServletException {
-		super.init(config);
-		SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
+		return "redirect:/addComputer/";
 	}
 }
--- src/main/java/com/excilys/servlet/EditComputerServlet.java
+++ src/main/java/com/excilys/servlet/EditComputerServlet.java
@@ -4,34 +4,26 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
 import com.excilys.mapper.ComputerMapper;
 import com.excilys.model.Company;
 import com.excilys.model.Computer;
-import com.excilys.persistence.CompanyDAO;
 import com.excilys.service.CompanyService;
 import com.excilys.service.ComputerService;
 
 @Controller
-@RequestMapping("/editComputer")
+@RequestMapping("/editComputer/{id}")
 public class EditComputerServlet  {
 	
 	private static final Logger LOG4J = LogManager.getLogger(EditComputerServlet.class.getName());
@@ -43,7 +35,7 @@ public class EditComputerServlet  {
 	@Autowired
 	private ComputerMapper computerMapper;
 
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET)
 	public String doGet(@PathVariable("id") long id, ModelMap model) throws ServletException, IOException {
 		LOG4J.info("Entering get method");
 		List<Company> companies = companyService.getCompanies();
@@ -60,7 +52,7 @@ public class EditComputerServlet  {
 		return "editComputer";
 	}
 	
-	@RequestMapping(value = "/{id}", method = RequestMethod.POST)
+	@RequestMapping(method = RequestMethod.POST)
 	public String doPost(@PathVariable("id") long id, HttpServletRequest request) throws ServletException, IOException {
 		String name = request.getParameter("computerName");
 		String introduced = request.getParameter("introduced");
@@ -73,11 +65,4 @@ public class EditComputerServlet  {
 		return "redirect:/editComputer/" + id;
 
 	}
-
-
-//	@Override
-//	public void init(ServletConfig config) throws ServletException {
-//		super.init(config);
-//		SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
-//	}
 }
